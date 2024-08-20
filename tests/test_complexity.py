673cc7820f961e2037df92f82f233ec86605280c
Victor Huynh <vhuynh4529@gmail.com>
2024-08-20 13:20:55 -0700

    added tests for ky dim and pesin entropy, pretty trivial though wanted for completeness~

--- tests/test_complexity.py
+++ tests/test_complexity.py
@@ -5,7 +5,7 @@ from dynadojo.systems.gilpin_flows import GilpinFlowsSystem
 import numpy as np
 import EntropyHub as EH
 from dynadojo.utils.julia_in_python import julia_find_lyapunov
-from dynadojo.utils.complexity_measures import multi_en, corr_dim, find_lyapunov_exponents
+from dynadojo.utils.complexity_measures import multi_en, corr_dim, find_lyapunov_exponents, kaplan_yorke_dimension, pesin
 
 ALL_FLOWS = [
     "Lorenz",
@@ -33,6 +33,34 @@ ALL_CANONCIAL = {
 
 flows = ALL_FLOWS  # To test multiple systems, add them to this list
 maps = ALL_MAPS
+
+# Creates lyapunov spectrum for Lorenz
+system = SystemChecker(GilpinFlowsSystem(latent_dim=3, embed_dim=3, system_name="Lorenz", seed=1))
+unwrapped_system = system._system
+model = unwrapped_system.system
+
+model.beta = 8/3
+model.rho = 28
+model.sigma = 10
+timesteps = 1000
+x0 = np.array([[-9.7869288, -15.03852, 20.533978]])
+
+xtpts, x = unwrapped_system.make_data(x0, timesteps=timesteps, return_times=True)
+lspectrum_calc = find_lyapunov_exponents(x[0], xtpts, timesteps, model)
+
+# Creates lyapunov spectrum for Rossler
+system = SystemChecker(GilpinFlowsSystem(latent_dim=3, embed_dim=3, system_name="Rossler", seed=1))
+unwrapped_system = system._system
+model = unwrapped_system.system
+
+model.a = 0.2
+model.b = 0.2
+model.c = 5.7
+timesteps = 1000
+x0 = np.array([[6.5134412, 0.4772013, 0.34164294]])
+
+xtpts, x = unwrapped_system.make_data(x0, timesteps=timesteps, return_times=True)
+rspectrum_calc = find_lyapunov_exponents(x[0], xtpts, timesteps, model)
 
 class TestCorrDim(unittest.TestCase):
 
@@ -63,42 +91,37 @@ class TestMultiEn(unittest.TestCase):
 class TestLyapunov(unittest.TestCase):
 
     def test_lorenz(self):
-        system = SystemChecker(GilpinFlowsSystem(latent_dim=3, embed_dim=3, system_name="Lorenz", seed=1))
-        unwrapped_system = system._system
-        model = unwrapped_system.system
-
-        model.beta = 8/3
-        model.rho = 28
-        model.sigma = 10
-        timesteps = 1000
-        x0 = np.array([[-9.7869288, -15.03852, 20.533978]])
-
-        xtpts, x = unwrapped_system.make_data(x0, timesteps=timesteps, return_times=True)
-        spectrum_calc = find_lyapunov_exponents(x[0], xtpts, timesteps, model)
-
         spectrum_canon = [0.906, 0, -14.572]
         spectrum_julia = julia_find_lyapunov("Lorenz", timesteps=timesteps, u0=[-9.7869288, -15.03852, 20.533978], p=[8/3, 28, 10])
 
-        np.testing.assert_allclose(spectrum_calc, spectrum_julia, rtol=0.1)
-        np.testing.assert_allclose(spectrum_calc, spectrum_canon, rtol=0.1)
+        np.testing.assert_allclose(lspectrum_calc, spectrum_julia, rtol=0.1)
+        np.testing.assert_allclose(lspectrum_calc, spectrum_canon, rtol=0.1)
 
     def test_rossler(self):
-        system = SystemChecker(GilpinFlowsSystem(latent_dim=3, embed_dim=3, system_name="Rossler", seed=1))
-        unwrapped_system = system._system
-        model = unwrapped_system.system
-
-        model.a = 0.2
-        model.b = 0.2
-        model.c = 5.7
-        timesteps = 1000
-        x0 = np.array([[6.5134412, 0.4772013, 0.34164294]])
-
-        xtpts, x = unwrapped_system.make_data(x0, timesteps=timesteps, return_times=True)
-        spectrum_calc = find_lyapunov_exponents(x[0], xtpts, timesteps, model)
-
         spectrum_canon = [0.0714, 0, -5.3943]
         spectrum_julia = julia_find_lyapunov("Rossler", timesteps=timesteps, u0=[6.5134412, 0.4772013, 0.34164294], p=[0.2, 0.2, 5.7])
 
-        np.testing.assert_allclose(spectrum_calc, spectrum_julia, rtol=0.1)
-        np.testing.assert_allclose(spectrum_calc, spectrum_canon, rtol=0.1)
-        
+        np.testing.assert_allclose(rspectrum_calc, spectrum_julia, rtol=0.1)
+        np.testing.assert_allclose(rspectrum_calc, spectrum_canon, rtol=0.1)
+
+class TestKYDim(unittest.TestCase):
+
+    def test_lorenz(self):
+        ky_dim_calc = kaplan_yorke_dimension(lspectrum_calc)
+        ky_dim_canon = 2.075158758095728
+        np.testing.assert_allclose(ky_dim_calc, ky_dim_canon, rtol=0.1)
+
+    def test_rossler(self):
+        ky_dim_calc = kaplan_yorke_dimension(rspectrum_calc)
+        ky_dim_canon = 2.0146095059018845
+        np.testing.assert_allclose(ky_dim_calc, ky_dim_canon, rtol=0.1)
+
+class TestPesinEn(unittest.TestCase):
+
+    def test_lorenz(self):
+        pesin_calc = pesin(lspectrum_calc)
+        np.testing.assert_(pesin_calc >= 0)
+
+    def test_rossler(self):
+        pesin_calc = pesin(rspectrum_calc)
+        np.testing.assert_(pesin_calc >= 0)
